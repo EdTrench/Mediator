3d0ae648011cbf4e861aa2a95820c466d83a75f2
ed_trench@yahoo.com <ed_trench@yahoo.com>
2015-06-19 14:38:28 +0100

    1

---
+++ Mediator/Framework/UserControls/Races.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Mediator.Framework.UserControls
+{
+    partial class Races
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblRider = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(44, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // lblRider
+            // 
+            this.lblRider.AutoSize = true;
+            this.lblRider.Location = new System.Drawing.Point(3, 6);
+            this.lblRider.Name = "lblRider";
+            this.lblRider.Size = new System.Drawing.Size(35, 13);
+            this.lblRider.TabIndex = 1;
+            this.lblRider.Text = "Rider:";
+            // 
+            // Races
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblRider);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Races";
+            this.Size = new System.Drawing.Size(287, 27);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblRider;
+    }
+}
